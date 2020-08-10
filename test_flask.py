49eff6937af799c31b7057f5584b269410d50f3e
diana <gaponcic.d@gmail.com>
2020-08-10 21:53:52 +0300

    minor change

--- test_flask.py
+++ test_flask.py
@@ -4,4 +4,4 @@ import requests
 def test_hello():
   response = requests.get('http://localhost:5000')
   assert response.status_code == 200
-  assert response.text == 'Hello, World'
+  assert response.text == 'Hello, World!'
